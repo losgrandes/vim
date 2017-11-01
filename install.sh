3eb7b722a36a465d85a2e57aee0e24f5b93ef9fc
losgrandes <pwajda@gmail.net.pl>
2017-11-01 09:58:24 +0100

    Revert "Support param -p|--projects-path."
    This reverts commit fc5c6aaf2ee2514669a08c8ce239de87ab1f1bc4.

--- install.sh
+++ install.sh
@@ -2,37 +2,21 @@
 
 VIMRC=~/.vim/.vimrc
 VIMRC_LINK=~/.vimrc
-PROJECTS_PATH=""
-
-while [[ $# -gt 0 ]]
-do
-key="$1"
-    
-case $key in
-    -p|--projects-path)
-    PROJECTS_PATH="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    -h|--help)
-    echo "$0 [ -p|--projects-path PATH ]"
-    exit
-    ;;
-esac
-done
+DEFAULT_REPOS=~/git/
 
 if [ -L $VIMRC_LINK ];then
     echo "$VIMRC_LINK already exists. Don't want to overwrite. Exiting!"
     exit
 fi
-
 sudo apt-get install vim vim-nox exuberant-ctags pylint 
 ln -s $VIMRC $VIMRC_LINK
 vim +BundleInstall +qall
 
-if [ ! -z $PROJECTS_PATH ];then
-    echo -n "Running ctags for $PROJECTS_PATH"
-    ctags -f ~/tags -R $PROJECTS_PATH
-    echo " Done!"
+echo -n "Need to scan for tags. Where're your repos? [$DEFAULT_REPOS]:"
+read dir
+if [ -z "$dir" ];then
+    dir="$DEFAULT_REPOS"
 fi
+eval dir=$dir # Translating ~ into $HOME
+ctags -f ~/tags -R "$dir"
 echo "All Done!"

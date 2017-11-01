fc5c6aaf2ee2514669a08c8ce239de87ab1f1bc4
losgrandes <pwajda@gmail.net.pl>
2017-11-01 09:08:11 +0100

    Support param -p|--projects-path.

--- install.sh
+++ install.sh
@@ -2,12 +2,30 @@
 
 VIMRC=~/.vim/.vimrc
 VIMRC_LINK=~/.vimrc
-DEFAULT_REPOS=~/git/
+PROJECTS_PATH=""
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+    
+case $key in
+    -p|--projects-path)
+    PROJECTS_PATH="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -h|--help)
+    echo "$0 [ -p|--projects-path PATH ]"
+    exit
+    ;;
+esac
+done
 
 if [ -L $VIMRC_LINK ];then
     echo "$VIMRC_LINK already exists. Don't want to overwrite. Exiting!"
     exit
 fi
+
 sudo apt-get install vim vim-nox exuberant-ctags pylint 
 ln -s $VIMRC $VIMRC_LINK
 vim +BundleInstall +qall
@@ -15,11 +33,9 @@ echo "\" Colorscheme" >> $VIMRC_LINK
 echo "colorscheme molokai" >> $VIMRC_LINK
 echo "let g:rehash256 = 1" >> $VIMRC_LINK
 
-echo -n "Need to scan for tags. Where're your repos? [$DEFAULT_REPOS]:"
-read dir
-if [ -z "$dir" ];then
-    dir="$DEFAULT_REPOS"
+if [ ! -z $PROJECTS_PATH ];then
+    echo -n "Running ctags for $PROJECTS_PATH"
+    ctags -f ~/tags -R $PROJECTS_PATH
+    echo " Done!"
 fi
-eval dir=$dir # Translating ~ into $HOME
-ctags -f ~/tags -R "$dir"
 echo "All Done!"
